1b1248008e2092373061cc773d00342eb8764c39
hyejoung <qkrgpwjd1996@gmail.com>
2025-07-17 17:01:54 +0900

    자바스크립트 완성

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,22 @@
 const magazin = new Swiper('.magazin .container_wrap',{
+    loop: true,
     slidesPerView:3,
 });
-const best = new Swiper('.best', {
-    slidesPerView: '4',
+new Swiper('.best', {
+    slidesPerView: 4,
+    spaceBetween: 20,
     loop: true,
-    }
-);
+    centeredSlides: false,
+});
+new Swiper('.reco', {
+    slidesPerView: 4,
+    spaceBetween: 20,
+    loop: true,
+    centeredSlides: false,
+});
+new Swiper('.all', {
+    slidesPerView: 4,
+    spaceBetween: 20,
+    loop: true,
+    centeredSlides: false,
+});
